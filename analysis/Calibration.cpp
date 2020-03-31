7ec781d72bf8dd8fde945b074be596669aee5270
Francesco Sciotti <francescosciotti@FrancesttisMBP3.homenet.telecomitalia.it>
2020-03-31 20:41:09 +0200

    abbiamo aggiunto la funzione di fit e creato gli istogrammi scalati al Emax atteso dall'effetto fotoelettrico

--- analysis/Calibration.cpp
+++ analysis/Calibration.cpp
@@ -9,15 +9,20 @@
 #define NMIN 0
 #define NMAX 1200
 #define NBIN 100
+#define MEDIA 661.7
 
 #include<iostream>
 #include<stdio.h>
-//#include<vector>
+#include<vector>
 
 #include"TFile.h"
 #include"TTree.h"
 #include"TString.h"
 #include"TH1F.h"
+#include"TAxis.h"
+#include"TF1.h"
+#include"TFitResult.h"
+#include"TFitResultPtr.h"
 #include"TCanvas.h"
 
 
@@ -54,36 +59,70 @@ int main(int argc, char* argv[]) {
   tree->SetBranchAddress("vcharge", &vcharge);
 
   int nEntries = tree->GetEntries();
-  float rapporto;
-  
+  int flag_fit = 1;
+
 
   std::string plotsDir(Form("Istogramma_spettro_Cesio/"));
   system( Form("mkdir -p %s", plotsDir.c_str()) );
-    
+
+  double xmin[NCH] = {950, 650, 650, 750, 850, 800, 700, 750, 700, 700, 900, 950, 700, 800, 700, 700};
+  double medie[NCH] ={0};
+  double k[NCH]= {0};
+
 
     for (int channel=0; channel<NCH; channel++) {
         
         TH1F* hist = new TH1F("hist",Form("Spettro Cesio [Ch: %d]", channel), NBIN, NMIN, NMAX);
-        
-        
+
         for (int entry=0; entry<nEntries ; entry++) {
             tree->GetEntry(entry);
             
             if(-vcharge[channel]>CMIN){
                 hist -> Fill(-vcharge[channel]);
             }
+
         }
+
+        TF1 *f1 = new TF1("f1", "gaus", xmin[channel], NMAX);
+
+        TFitResultPtr r = hist->Fit("f1", "SRQ");
+
+        medie[channel] = r->Parameter(1);
         
+
+        k[channel]= MEDIA/medie[channel];
+
+        TH1F* hist_scaled = new TH1F("hist_scaled",Form("Spettro Cesio calibrato [Ch: %d]", channel), NBIN, NMIN*k[channel], NMAX*k[channel]);
+        
+        for (int entry=0; entry<nEntries ; entry++) {
+          tree->GetEntry(entry);
+          if(-vcharge[channel]>CMIN){
+             hist_scaled -> Fill(-vcharge[channel]*k[channel]);
+          }
+        }
+
         TCanvas* E = new TCanvas("hist","Spettro Cesio",600,800); // Nome, Titolo,x,y
-        
         E -> cd(); // Apre una sessione
-        
         hist -> Draw(); // Disegna l'istogramma
-        
         E -> SaveAs(Form("%s/Istogramma_Spettro_Cesio_%d.pdf", plotsDir.c_str(),channel));
-        
-        delete hist, E;
+
+        TCanvas* F = new TCanvas("hist","Spettro Cesio Scalato",600,800); // Nome, Titolo,x,y
+        F -> cd(); // Apre una sessione
+        hist_scaled -> Draw(); // Disegna l'istogramma
+        F -> SaveAs(Form("%s/Istogramma_Spettro_Cesio_%d_Calibrato.pdf", plotsDir.c_str(),channel));
+
+
+        delete hist;
+        delete hist_scaled;
+
     }
+
+
+
+
+
+
+
     
     return 0;
 
