4620e566eabfcf5c16568ff623b9c0bc809482a1
Francesco Sciotti <sciotti.1757449@studenti.uniroma1.it>
2020-04-27 19:53:24 +0200

    finale fra

--- analysis/calibrazione_energia_finale.cpp
+++ analysis/calibrazione_energia_finale.cpp
@@ -26,6 +26,7 @@
 #include"TGraphErrors.h"
 #include"THistPainter.h"
 #include"TMultiGraph.h"
+#include"TAttLine.h"
 
 
 
@@ -33,195 +34,219 @@
 
 int main(int argc, char* argv[]) {
 
-	std::ifstream efs (argv[2], std::ifstream::in);
-	std::ifstream ofs (argv[3], std::ifstream::in);
+  std::ifstream efs (argv[2], std::ifstream::in);
+  std::ifstream ofs (argv[3], std::ifstream::in);
 
-	//QUESTI DUE VANNO PASSATI DAL PROGRAMMA DEL FIT--PARI
-	double picco_FE_p[NCH]={0.};
-	double inc_picco_FE_p[NCH]={0.};
+  //QUESTI DUE VANNO PASSATI DAL PROGRAMMA DEL FIT--PARI
+  double picco_FE_p[NCH]={0.};
+  double inc_picco_FE_p[NCH]={0.};
 
-	//QUESTI DUE VANNO PASSATI DAL PROGRAMMA DEL FIT--DISPARI
-	double picco_FE_d[NCH]={0.};
-	double inc_picco_FE_d[NCH]={0.};
+  //QUESTI DUE VANNO PASSATI DAL PROGRAMMA DEL FIT--DISPARI
+  double picco_FE_d[NCH]={0.};
+  double inc_picco_FE_d[NCH]={0.};
+  for(int i=0;i<NCH;i++){
+    efs >>picco_FE_p[i]>>inc_picco_FE_p[i];
+  }
+  for(int i=0;i<NCH;i++){
+    ofs >>picco_FE_d[i]>>inc_picco_FE_d[i];
+  }
 
-	for(int i=0;i<NCH;i++) efs >>picco_FE_p[i]>>inc_picco_FE_p[i];
-	for(int i=0;i<NCH;i++) ofs >>picco_FE_d[i]>>inc_picco_FE_d[i];
+
+  TString rootFileName(argv[1]);
+  TFile* rootFile = new TFile(rootFileName);
+  std::cout<<"Reading data from root file "<<argv[1]<<std::endl;
+
+  TTree* tree = (TTree*) rootFile->Get("tree");
+  if(!tree) {
+    std::cout<<"Error, no tree called tree in "<<argv[1 ]<<". Exiting."<<std::endl;
+    exit(ERROR_NOTREE);
+  }
+
+  int ev;
+  float base[NCH], vcharge[NCH];
+
+  tree->SetBranchAddress("ev", &ev);
+  tree->SetBranchAddress("base", &base);
+  tree->SetBranchAddress("vcharge", &vcharge);
+
+  int nEntries = tree->GetEntries();
+
+  std::string plotsDir(Form("Istogramma_spettro_Cesio/"));
+  system( Form("mkdir -p %s", plotsDir.c_str()) );
+
+  double k_cal[NCH] = {0.}; //k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER CALIBRARE
+  double inc_k_cal[NCH] = {0.};
+  double k_medio_cal = 0;
+  double inc_k_medio_cal = 0;
+  double picco_test[NCH] = {0.};  //k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER TESTARE
+  double inc_picco_test[NCH] = {0.};
+  double k_medio_test = 0;
+  double inc_k_medio_test = 0;
+  double k_test[NCH] = {0.};  //k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER TESTARE
+  double inc_k_test[NCH] = {0.};
+
+  for(int channel=0; channel<NCH; channel++){
+
+    k_cal[channel] = MEDIA/picco_FE_p[channel];
+    inc_k_cal[channel] = ((k_cal[channel]/picco_FE_p[channel])*(k_cal[channel]/picco_FE_p[channel]))*(inc_picco_FE_p[channel]*inc_picco_FE_p[channel]); //ERRORE QUADRATO PROPAGATO
+
+    k_medio_cal += k_cal[channel];
+    inc_k_medio_cal += inc_k_cal[channel];
+
+    picco_test[channel] = picco_FE_d[channel]*k_cal[channel];
+    inc_picco_test[channel] = (k_cal[channel]*k_cal[channel])*(inc_picco_FE_d[channel])*(inc_picco_FE_d[channel]) + (picco_FE_d[channel]*picco_FE_d[channel])*(inc_k_cal[channel]);
+
+    k_test[channel] = MEDIA/picco_test[channel];
+    inc_k_test[channel] = ((k_test[channel]/picco_test[channel])*(k_test[channel]/picco_test[channel]))*(inc_picco_test[channel]); //ERRORE QUADRATO PROPAGATO
+
+    k_medio_test += k_test[channel];
+    inc_k_medio_test += inc_k_test[channel];
+
+  }
+
+  k_medio_cal = k_medio_cal/NCH;
+  inc_k_medio_cal = sqrt(inc_k_medio_cal)/NCH;
 
 
-	TString rootFileName(argv[1]);
-	TFile* rootFile = new TFile(rootFileName);
-	std::cout<<"Reading data from root file "<<argv[1]<<std::endl;
+  k_medio_test = k_medio_test/NCH;
+  inc_k_medio_test = sqrt(inc_k_medio_test)/NCH;
 
-	TTree* tree = (TTree*) rootFile->Get("tree");
-	if(!tree) {
-		std::cout<<"Error, no tree called tree in "<<argv[1 ]<<". Exiting."<<std::endl;
-		exit(ERROR_NOTREE);
-	}
+  Double_t channel_axis[NCH] = {0.};
 
-	int ev;
-	float base[NCH], vcharge[NCH];
+  Double_t k_cal_normalizzato[NCH] = {0.};
+  Double_t inc_k_cal_normalizzato[NCH] = {0.};
 
-	tree->SetBranchAddress("ev", &ev);
-	tree->SetBranchAddress("base", &base);
-	tree->SetBranchAddress("vcharge", &vcharge);
+  Double_t k_test_normalizzato[NCH] = {0.};
+  Double_t inc_k_test_normalizzato[NCH] = {0.};
 
-	int nEntries = tree->GetEntries();
-	int flag_fit = 1;
+  Double_t *err_channel=0; // Dummy
 
-	std::string plotsDir(Form("Istogramma_spettro_Cesio/"));
-	system( Form("mkdir -p %s", plotsDir.c_str()) );
 
-	double k_cal[NCH] = {0.};	//k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER CALIBRARE
-	double inc_k_cal[NCH] = {0.};
-	double k_medio_cal = 0;
-	double inc_k_medio_cal = 0;
-	double picco_test[NCH] = {0.};	//k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER TESTARE
-	double inc_picco_test[NCH] = {0.};
-	double k_medio_test = 0;
-	double inc_k_medio_test = 0;
-	double k_test[NCH] = {0.};	//k sarà il rapporto tra la MEDIA  e il picco FE per ogni canale QUA PER TESTARE
-	double inc_k_test[NCH] = {0.};
+  for(int channel=0; channel<NCH; channel++){
 
-	for(int channel=0; channel<NCH; channel++){
+    channel_axis[channel]=channel+1; 
 
-		k_cal[channel] = MEDIA/picco_FE_p[channel];
-    	inc_k_cal[channel] = ((k_cal[channel]/picco_FE_p[channel])*(k_cal[channel]/picco_FE_p[channel]))*(inc_picco_FE_p[channel]*inc_picco_FE_p[channel]); //ERRORE QUADRATO PROPAGATO
+    k_cal_normalizzato[channel]=k_cal[channel]/k_medio_cal;
+    inc_k_cal_normalizzato[channel]= inc_k_cal[channel]/(k_medio_cal*k_medio_cal) + (k_cal_normalizzato[channel]*k_cal_normalizzato[channel])/(k_medio_cal*k_medio_cal)*(inc_k_medio_cal*inc_k_medio_cal);
+    inc_k_cal_normalizzato[channel] = sqrt(inc_k_cal_normalizzato[channel]);
 
-    	k_medio_cal += k_cal[channel];
-    	inc_k_medio_cal += inc_k_cal[channel];
+    k_test_normalizzato[channel]=k_test[channel]/k_medio_test;
+    inc_k_test_normalizzato[channel]=inc_k_test[channel]/(k_medio_test*k_medio_test) + (k_test_normalizzato[channel]*k_test_normalizzato[channel])/(k_medio_test*k_medio_test)*(inc_k_medio_test*inc_k_medio_test);
+    inc_k_test_normalizzato[channel] = sqrt(inc_k_test_normalizzato[channel]);
+    std::cout <<"channel: "<< channel << "K: " << k_test_normalizzato[channel] << "Inc: " << inc_k_test_normalizzato[channel] << std::endl;
+  }
 
-    	picco_test[channel] = picco_FE_d[channel]*k_cal[channel];
-    	inc_picco_test[channel] = (k_cal[channel]*k_cal[channel])*(inc_picco_FE_d[channel])*(inc_picco_FE_d[channel]) + (picco_FE_d[channel]*picco_FE_d[channel])*(inc_k_cal[channel]*inc_k_cal[channel]);
 
-    	k_test[channel] = MEDIA/picco_test[channel];
-    	inc_k_test[channel] = ((k_test[channel]/picco_test[channel])*(k_test[channel]/picco_test[channel]))*(inc_picco_test[channel]*inc_picco_test[channel]); //ERRORE QUADRATO PROPAGATO
 
-    	k_medio_test += k_test[channel];
-    	inc_k_medio_test += inc_k_test[channel];
 
+
+
+
+  TCanvas* c2 = new TCanvas("c2", "Grafico Calibrazione Energie");
+  c2->cd();
+  TMultiGraph *mg = new TMultiGraph();
+  TGraphErrors* gr=new TGraphErrors(NCH, channel_axis, k_test_normalizzato, err_channel, inc_k_test_normalizzato);
+
+  TF1 *f = new TF1("f", "[0]"); // funzione costante
+  gr->Fit("f", "QN"); // fit sul grafico
+  double_t chi2 = gr->Chisquare(f);   // Faccio il chi 2 sul modello cost = 1
+  std::cout<< " chi-square test returned a value of chi-square/N dof equal to: " << chi2/f->GetNDF()<< " with Ndof: "<< f->GetNDF()<<std::endl;
+  gr->SetMarkerStyle(21);
+  gr->SetMarkerSize(0.3);
+  TGraphErrors* gr1=new TGraphErrors(NCH, channel_axis, k_cal_normalizzato, err_channel, inc_k_cal_normalizzato);
+  gr1->SetMarkerColorAlpha(kRed, 1);
+  gr1->SetMarkerStyle(20);
+  mg->GetXaxis()->SetTitle("Canale");
+  mg->GetYaxis()->SetTitle("k Normalizzato");
+
+  mg->Add(gr1);
+  mg->Add(gr);
+  mg->Draw("AP");
+
+  c2->SaveAs(Form("%s/calibrazione_energie.pdf", plotsDir.c_str()));
+
+
+
+
+
+
+
+
+
+
+
+
+    //A E B DEVONO ESSERE I DUE CANALI CHE GLI PASSIAMO
+
+  int A, B;
+
+    // Si fa passare i due canali da riga di comando
+
+  std::cout<<"Scegliere i due canali per la sovrapposizione delgi istogrammi:"<<std::endl;
+  std::cout<<"Canale: ";
+  std::cin>>A;
+  std::cout<<"\nCanale: ";
+  std::cin>>B;
+  std::cout<<std::endl;
+
+    // istogrammi da sovrapporre
+    // prima...
+
+  TH1F* hist1 = new TH1F("hist1",Form("Spettro Cesio [Ch: %d]", A), NBIN, NMIN, NMAX);
+  TH1F* hist2 = new TH1F("hist2",Form("Spettro Cesio [Ch: %d]", B), NBIN, NMIN, NMAX);
+
+    // scalati
+    // dopo...
+
+  TH1F* hist1_scaled = new TH1F("hist1_scaled",Form("Spettro Cesio scalato [Ch: %d]", A), NBIN, k_cal[A]*NMIN, k_cal[A]*NMAX);
+  TH1F* hist2_scaled = new TH1F("hist2_scaled",Form("Spettro Cesio scalato [Ch: %d]", B), NBIN, k_cal[B]*NMIN, k_cal[B]*NMAX);  
+
+  for (int entry=0; entry<nEntries/2; entry++) {
+    tree->GetEntry(entry*2+1);
+
+    if(-vcharge[A]>CMIN){
+      hist1 -> Fill(-vcharge[A]);
+      hist1_scaled -> Fill(-vcharge[A]*k_cal[A]);
     }
 
-    k_medio_cal = k_medio_cal/NCH;
-    inc_k_medio_cal = sqrt(inc_k_medio_cal)/NCH;
-    std::cout<<k_medio_cal<<std::endl;
+    if(-vcharge[B]>CMIN){
+      hist2 -> Fill(-vcharge[B]);
+      hist2_scaled -> Fill(-vcharge[B]*k_cal[B]);
+    }
 
-    k_medio_test = k_medio_test/NCH;
-    inc_k_medio_test = sqrt(inc_k_medio_test)/NCH;
+  }  
 
-    Double_t channel_axis[NCH];
+  TCanvas* H = new TCanvas("hist1","Sovrapposizione Istogrammi non Scalati"); // Nome, Titolo,x,y
+  hist1->SetTitle(";Carica (pC);Numero Eventi");
+  hist1->GetYaxis()->SetRangeUser(0,2500);
+  H -> cd(); // Apre una sessione
+  hist1 -> Draw();  
+  hist2->SetLineColor(kRed);
+  hist2 -> Draw("SAME");
+  H -> Update();
+  H -> SaveAs(Form("%s/Istogrammi_ch%d_ch%d_sovrapposti.pdf", plotsDir.c_str(), A, B));
 
-    Double_t k_cal_normalizzato[NCH];
-    Double_t inc_k_cal_normalizzato[NCH];
+  delete hist1;
+  delete hist2;
 
-    Double_t k_test_normalizzato[NCH];
-    Double_t inc_k_test_normalizzato[NCH];
+  TCanvas* I = new TCanvas("hist1_scaled","Sovrapposizione Istogrammi Scalati"); // Nome, Titolo,x,y
+  hist1_scaled->SetTitle(";Energia (MeV);Numero Eventi");
+  hist1_scaled->GetYaxis()->SetRangeUser(0,2500);
+  I -> cd(); // Apre una sessione
+  hist1_scaled -> Draw();
+  hist2_scaled->SetLineColor(kRed);
+  hist2_scaled -> Draw("SAME");
 
-  	Double_t *err_channel=0; // Dummy
-
-
-  	for(int channel=0; channel<NCH; channel++){
-
-  		channel_axis[channel]=channel+1; 
-
-  		k_cal_normalizzato[channel]=k_cal[channel]/k_medio_cal;
-  		inc_k_cal_normalizzato[channel]= inc_k_cal[channel]/(k_medio_cal*k_medio_cal) + (k_cal_normalizzato[channel]*k_cal_normalizzato[channel])/(k_medio_cal*k_medio_cal)*(inc_k_medio_cal*inc_k_medio_cal);
-  		inc_k_cal_normalizzato[channel] = sqrt(inc_k_cal_normalizzato[channel]);
-
-  		k_test_normalizzato[channel]=k_test[channel]/k_medio_test;
-  		inc_k_test_normalizzato[channel]=inc_k_test[channel]/(k_medio_test*k_medio_test) + (k_test_normalizzato[channel]*k_test_normalizzato[channel])/(k_medio_test*k_medio_test)*(inc_k_medio_test*inc_k_medio_test);
-  		inc_k_test_normalizzato[channel] = sqrt(inc_k_test_normalizzato[channel]);  	
-
-  	}
-
-  	TCanvas* c2 = new TCanvas("c2", "Grafico Calibrazione Energie");
-  	c2->cd();
-  	TMultiGraph *mg = new TMultiGraph();
-  	TGraphErrors* gr=new TGraphErrors(NCH, channel_axis, k_test_normalizzato, err_channel, inc_k_test_normalizzato);
-  	gr->GetXaxis()->SetTitle("Canale");
-  	gr->GetYaxis()->SetTitle("k Normalizzato");
-  	gr->SetMarkerStyle(21);
-  	gr->SetMarkerSize(1.0);
-  	TGraphErrors* gr1=new TGraphErrors(NCH, channel_axis, k_cal_normalizzato, err_channel, inc_k_cal_normalizzato);
-  	gr1->SetMarkerColorAlpha(kRed, 1);
-  	gr1->SetMarkerStyle(20);
-
-  	mg->Add(gr1);
-  	mg->Add(gr);
-  	mg->Draw("AP");
-
-  	c2->SaveAs(Form("%s/calibrazione_energie.pdf", plotsDir.c_str()));
+  I -> Update();
+  I -> SaveAs(Form("%s/Istogrammi_ch%d_ch%d_scalati_sovrapposti.pdf", plotsDir.c_str(), A, B));
 
 
+  delete hist1_scaled;
+  delete hist2_scaled;
 
 
-
-  	//A E B DEVONO ESSERE I DUE CANALI CHE GLI PASSIAMO
-
-  	int A, B;
-
-  	// Si fa passare i due canali da riga di comando
-
-  	std::cout<<"Scegliere i due canali per la sovrapposizione delgi istogrammi:"<<std::endl;
-  	std::cout<<"Canale: ";
-  	std::cin>>A;
-  	std::cout<<"\nCanale: ";
-  	std::cin>>B;
-  	std::cout<<std::endl;
-
-  	// istogrammi da sovrapporre
-  	// prima...
-
-  	TH1F* hist1 = new TH1F("hist1",Form("Spettro Cesio [Ch: %d]", A), NBIN, NMIN, NMAX);
-  	TH1F* hist2 = new TH1F("hist2",Form("Spettro Cesio [Ch: %d]", B), NBIN, NMIN, NMAX);
-
-  	// scalati
-  	// dopo...
-
-  	TH1F* hist1_scaled = new TH1F("hist1_scaled",Form("Spettro Cesio scalato [Ch: %d]", A), NBIN, k_cal[A]*NMIN, k_cal[A]*NMAX);
-  	TH1F* hist2_scaled = new TH1F("hist2_scaled",Form("Spettro Cesio scalato [Ch: %d]", B), NBIN, k_cal[B]*NMIN, k_cal[B]*NMAX);	
-
-  	for (int entry=0; entry<nEntries/2; entry++) {
-  		tree->GetEntry(entry*2+1);
-
-  		if(-vcharge[A]>CMIN){
-  			hist1 -> Fill(-vcharge[A]);
-  			hist1_scaled -> Fill(-vcharge[A]*k_cal[A]);
-  		}
-
-  		if(-vcharge[B]>CMIN){
-  			hist2 -> Fill(-vcharge[B]);
-  			hist2_scaled -> Fill(-vcharge[B]*k_cal[B]);
-  		}
-
-  	}  
-
-  	TCanvas* H = new TCanvas("hist1","Sovrapposizione Istogrammi non Scalati"); // Nome, Titolo,x,y
-  	hist1->SetTitle(";Carica (pC);Numero Eventi");
-  	hist1->GetYaxis()->SetRangeUser(0,2500);
-  	H -> cd(); // Apre una sessione
-  	hist1 -> Draw();
-  	hist2 -> Draw("SAME");
-  	H -> Update();
-  	H -> SaveAs(Form("%s/Istogrammi_ch%d_ch%d_sovrapposti.pdf", plotsDir.c_str(), A, B));
-
-  	delete hist1;
-  	delete hist2;
-
-	TCanvas* I = new TCanvas("hist1_scaled","Sovrapposizione Istogrammi Scalati"); // Nome, Titolo,x,y
-  	hist1_scaled->SetTitle(";Energia (MeV);Numero Eventi");
-  	hist1_scaled->GetYaxis()->SetRangeUser(0,2500);
-  	I -> cd(); // Apre una sessione
-  	hist1_scaled -> Draw();
-  	hist2_scaled -> Draw("SAME");
-  	I -> Update();
-  	I -> SaveAs(Form("%s/Istogrammi_ch%d_ch%d_scalati_sovrapposti.pdf", plotsDir.c_str(), A, B));
-
-
-  	delete hist1_scaled;
-  	delete hist2_scaled;
-
-  	return 0;
-  }
+  return 0;
+}
 
 
 
