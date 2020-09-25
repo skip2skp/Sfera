7b2003a4c9ce4b7e078322043d9ecd820a10f838
Andreaq40 <quaranta.1719022@studenti.uniroma1.it>
2020-09-25 16:06:15 +0200

    I file boundaries....txt contengono i parametri per il fit gaussiano sugli istogrammi - endpoint dello spettro, estremi del range su cui eseguire il fit. Abbiamo inserito i parametri per l'ultima presa dati da 2M e la presa dati da 500k eventi sul Cesio. calibration.cpp legge dai file questi parametri.

--- analysis/calibration.cpp
+++ analysis/calibration.cpp
@@ -42,8 +42,8 @@
 
 int main(int argc, char* argv[]) {
 
-	if(argc!=2) {
-		std::cout<<"Usage: ./myexe.exe myfile.root"<<std::endl;
+	if(argc!=3) {
+		std::cout<<"Usage: ./myexe.exe myfile.root filewith.boundaries"<<std::endl;
 		exit(ERROR_USAGE);
 	}
 
@@ -79,9 +79,23 @@ int main(int argc, char* argv[]) {
  	 system( Form("mkdir -p %s", plotsDir.c_str()) );
 
 
-  	double xmin[15]={1300,470,1450,700,700,380,1250,1000,1600,950,650,1350,550,2200,390}; //... to be initialized // starting points
-  	double xmax[15]={1700,600,1800,1000,900,500,1500,1200,2000,1150,900,1700,700,2800,500};	//... to be initialized // end points
+ 	 double endpoint;//={2000, 700, 2000, 1200, 1100, 700, 1800, 1500, 2200, 1400, 1400, 1800, 2000, 1000, 3000, 1000};
+ 	 int bin;//={120, 180, 120, 120, 180, 180, 120, 120, 110, 120, 180, 110, 180, 100, 180};
+  	double xmin;//={1300,470,1450,950,700,380,1250,1000,1600,950,1400,1350,550,2200,390}; //... to be initialized // starting points
+  	double xmax;//={1700,600,1800,1150,900,500,1500,1200,1400,1150,1600,1700,700,2800,500};	//... to be initialized // end points
 
+
+  	//conviene passarli da file
+
+  	std::ifstream input_f;
+  	std::string inf_name=argv[2];
+  	input_f.open(inf_name);
+  	if(!input_f.is_open()) {
+
+		std::cout<<"Error: No file called "<<inf_name<<". Exiting."<<std::endl;
+  		exit(ERROR_NOFILE);
+
+  	} //check if file is open  	
 
   	std::ofstream out_k; // output file for calibration constants
   	std::string outf_name="calibration_const.dat";
@@ -90,6 +104,7 @@ int main(int argc, char* argv[]) {
   		std::cout<<"Error: could open file "<<outf_name<<". Exiting."<<std::endl;
   		exit(ERROR_NOFILE);
   	} //check if file is open
+
         std::ofstream out_fit; // output file for fit results
   	std::string outf_name2="par_fit.dat";
   	out_fit.open(outf_name2);
@@ -97,6 +112,7 @@ int main(int argc, char* argv[]) {
   		std::cout<<"Error: could open file "<<outf_name<<". Exiting."<<std::endl;
   		exit(ERROR_NOFILE);
   	} //check if file is open
+
   	out_k<<"# ch.\tk\terr_k"<<std::endl; // header
 	out_fit<<"#amp----mean----var----mean_err----var_err----chi2/Ndf"<< std::endl;
   	Double_t amp=0., mean=0., mean_err=0., var=0., var_err=0., chi2=0., Ndf=0.; // gaussian fit parameters
@@ -105,8 +121,14 @@ int main(int argc, char* argv[]) {
 
 	for (int channel=0; channel<NCH; channel++) {
 
-		//TH1F* spettro = new TH1F("spettro","", NBIN, 0, xmax[channel]);
-		TH1F* spettro = new TH1F("spettro","", NBIN, 0, 3000);
+		input_f>>endpoint;
+  		//input_f>>bin;
+  		input_f>>xmin;
+  		input_f>>xmax; // reads boundaries for fit on the fly from input file
+
+		//TH1F* spettro = new TH1F("spettro","", NBIN, 0, bin);
+		TH1F* spettro = new TH1F("spettro","", 100, 100, endpoint);
+
 
     	for (int entry=0; entry<nEntries; entry++) {
 
@@ -114,20 +136,21 @@ int main(int argc, char* argv[]) {
 
         	if(-vcharge[channel]>CMIN){spettro -> Fill(-vcharge[channel]);} //riempi l'isto
     } // end for on entries
-       		TF1 *fitgaus = new TF1("fitgaus", "gaus", xmin[channel], xmax[channel]); //fit gaussiana 1
+       		TF1 *fitgaus = new TF1("fitgaus", "gaus", xmin, xmax); //fit gaussiana 1
 
-					fitgaus->SetParameter(1, (xmin[channel]+xmax[channel])/2.);
+				fitgaus->SetParameter(1, (xmin+xmax)/2.);
 
     			TFitResultPtr gaussian_fit = spettro->Fit("fitgaus", "SRQ");  //“Q” Quiet mode  “S” result in TFitResultPtr ""R” Use the range
+				
 				amp =gaussian_fit->Parameter(1);
    				mean =gaussian_fit->Parameter(1);
    				var =gaussian_fit->Parameter(2);//first estimate of mean and sigma for finer fit
 				mean_err = gaussian_fit->ParError(1);
-	    			var_err = gaussian_fit->ParError(2);
-	    			chi2 =gaussian_fit->Chi2();
-	    			Ndf = gaussian_fit->Ndf(); //all the parameters
+	    		var_err = gaussian_fit->ParError(2);
+	    		chi2 =gaussian_fit->Chi2();
+	    		Ndf = gaussian_fit->Ndf(); //all the parameters
 	    			
-	    			out_fit<<amp<<"\t"<<mean<<"\t"<<var<<"\t"<<mean_err<<"\t"<<var_err<<"\t"<<chi2/Ndf<< std::endl;
+	    		out_fit<<amp<<"\t"<<mean<<"\t"<<var<<"\t"<<mean_err<<"\t"<<var_err<<"\t"<<chi2/Ndf<< std::endl;
 	    			
 	    			
     	//fitgaus->SetRange(mean-var, mean+2*var); // Set range of fit around the mean returned by the first. The range is asymmetrical, being larger on the right.
@@ -136,7 +159,7 @@ int main(int argc, char* argv[]) {
 		
    			
    			
-   		/*	TF1* fitgauss2= new TF1("fitgauss2", "gaus", xmin[channel], xmax[channel]);
+   		/*	TF1* fitgauss2= new TF1("fitgauss2", "gaus", xmin, xmax);
       
     fitgauss2->SetParameter(0,amp);
     fitgauss2->SetParameter(1,mean);
@@ -189,7 +212,7 @@ l->Draw("same");
 	    	k=mean/PEAK; // calibration constant // pC/keV
 	    	k_err=mean_err/PEAK; // error // pC/keV
 
-	    	out_k<<channel<<"\t"<<k<<"\t"<<k_err<<"\t"<<std::endl; // prints on file
+	    	out_k<<channel<<"\t\t"<<k<<"\t\t"<<k_err<<"\t\t"<<std::endl; // prints on file
 
 	
 
@@ -197,8 +220,11 @@ l->Draw("same");
 
 	std::cout<<"Printed calibration constants on file"<<outf_name<<" ."<<std::endl;
 
+	out_fit.close();
+	out_fit.close();
+	input_f.close();
+
 	return(0);
 
-	out_fit.close();
-	out_fit.close();
+	
 }
